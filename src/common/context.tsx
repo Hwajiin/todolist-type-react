0a5b63bdd555445d939cd90bad8a7a765a2b2453
Hwajiin <borderline0px@gmail.com>
2021-10-21 18:51:23 +0900

    Make todo reducer and context, provider

---
+++ src/common/context.tsx
@@ -0,0 +1,59 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useReducer,
+  useState,
+} from "react";
+import toDoReducer, { ToDoAction } from "./reducer";
+
+/**
+ * ToDoState의 형태
+ */
+type ToDo = {
+  id: string;
+  text: string;
+};
+
+export type ToDoState = {
+  toDos: ToDo[];
+  completed: ToDo[];
+};
+
+/**
+ * ToDoContext Type
+ */
+type ToDoContext = {
+  todoState: ToDoState;
+  dispatch: Dispatch<ToDoAction>;
+};
+
+const ToDoContext = createContext<ToDoContext | null>(null);
+
+const initialToDoState: ToDoState = {
+  toDos: [],
+  completed: [],
+};
+
+const ToDoProvider = ({ children }: { children: React.ReactNode }) => {
+  const [todoState, dispatch] = useReducer(toDoReducer, initialToDoState);
+
+  return (
+    <ToDoContext.Provider value={{ todoState, dispatch }}>
+      {children}
+    </ToDoContext.Provider>
+  );
+};
+
+const useDispatch = () => {
+  const { dispatch } = useContext(ToDoContext)!;
+  return dispatch;
+};
+
+const useToDoState = () => {
+  const { todoState } = useContext(ToDoContext)!;
+  return todoState;
+};
+
+export default ToDoProvider;
---
+++ src/common/reducer.tsx
@@ -0,0 +1,51 @@
+import { ToDoState } from "./context";
+import { v4 as uuidv4 } from "uuid";
+
+export type ToDoAction =
+  | { type: "ADD"; text: string }
+  | { type: "DEL"; id: string }
+  | { type: "COMPLETE"; id: string }
+  | { type: "UNCOMPLETE"; id: string }
+  | { type: "EDIT"; id: string; text: string };
+
+const toDoReducer = (state: ToDoState, action: ToDoAction): ToDoState => {
+  const { toDos, completed } = state;
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        toDos: [{ text: action.text, id: uuidv4() }, ...toDos],
+      };
+    case "DEL":
+      return {
+        ...state,
+        toDos: toDos.filter((todo) => todo.id !== action.id),
+        completed: completed.filter((todo) => todo.id !== action.id),
+      };
+    case "COMPLETE":
+      const completeTarget = toDos.find((todo) => todo.id === action.id)!;
+      return {
+        ...state,
+        toDos: toDos.filter((todo) => todo.id !== action.id),
+        completed: [completeTarget, ...completed],
+      };
+    case "UNCOMPLETE":
+      const unCompletedTarget = completed.find(
+        (todo) => todo.id === action.id
+      )!;
+      return {
+        ...state,
+        completed: completed.filter((todo) => todo.id !== action.id),
+        toDos: [unCompletedTarget, ...toDos],
+      };
+    // Edit 기능 구현하기
+    case "EDIT":
+      return {
+        ...state,
+      };
+    default:
+      throw new Error("Sorry");
+  }
+};
+
+export default toDoReducer;
