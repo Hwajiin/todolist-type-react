adfd14c8b5da2d96ea3ee73e24e768845e37332c
Hwajiin <borderline0px@gmail.com>
2021-10-22 20:24:01 +0900

    ToDoItem Component: Make onDelete, onToggle function with useDispatch

--- src/components/todo-item/todoItemContainer.tsx
+++ src/components/todo-item/todoItemContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ToDo } from "../../common/context";
+import { ToDo, useDispatch } from "../../common/context";
 import ToDoItemPresenter from "./todoItemPresenter";
 
 type ToDoItemContainerProps = {
@@ -7,7 +7,27 @@ type ToDoItemContainerProps = {
 };
 
 const ToDoItemContainer = ({ todo }: ToDoItemContainerProps) => {
-  return <ToDoItemPresenter todo={todo} />;
+  const dispatch = useDispatch();
+
+  const onDelete = () => {
+    dispatch({ type: "DEL", id: todo.id });
+  };
+
+  const onToggle = () => {
+    dispatch({ type: "COMPLETE", id: todo.id });
+  };
+
+  // Edit 기능 구현 후
+  const onEdit = () => {};
+
+  return (
+    <ToDoItemPresenter
+      todo={todo}
+      onDelete={onDelete}
+      onToggle={onToggle}
+      onEdit={onEdit}
+    />
+  );
 };
 
 export default ToDoItemContainer;
--- src/components/todo-item/todoItemPresenter.tsx
+++ src/components/todo-item/todoItemPresenter.tsx
@@ -3,18 +3,26 @@ import { ToDo } from "../../common/context";
 
 type ToDoItemPresenterProps = {
   todo: ToDo;
+  onDelete: () => void;
+  onToggle: () => void;
+  onEdit: () => void;
 };
 
-const ToDoItemPresenter = ({ todo }: ToDoItemPresenterProps) => {
+const ToDoItemPresenter = ({
+  todo,
+  onDelete,
+  onToggle,
+  onEdit,
+}: ToDoItemPresenterProps) => {
   return (
     <li>
       <div>
         <span>{todo.text}</span>
       </div>
       <div>
-        <button>DEL</button>
-        <button>COMPLETE</button>
-        <button>EDIT</button>
+        <button onClick={onDelete}>DEL</button>
+        <button onClick={onToggle}>COMPLETE</button>
+        <button onClick={onEdit}>EDIT</button>
       </div>
     </li>
   );
