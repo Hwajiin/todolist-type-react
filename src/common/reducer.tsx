3176f8152f52124d64f466c4b06ea6f368667066
Hwajiin <borderline0px@gmail.com>
2021-10-23 21:12:54 +0900

    Todo reducer: Impletement edit function

--- src/common/reducer.tsx
+++ src/common/reducer.tsx
@@ -40,8 +40,15 @@ const toDoReducer = (state: ToDoState, action: ToDoAction): ToDoState => {
       };
     // Edit 기능 구현하기
     case "EDIT":
+      const editToDoTarget = toDos.find((todo) => todo.id === action.id)!;
+      const editCompletedTarget = toDos.find((todo) => todo.id === action.id)!;
       return {
         ...state,
+        toDos: [{ ...editToDoTarget, text: action.text }, ...toDos],
+        completed: [
+          { ...editCompletedTarget, text: action.text },
+          ...completed,
+        ],
       };
     default:
       throw new Error("Sorry");
